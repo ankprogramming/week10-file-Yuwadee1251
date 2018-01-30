73462894cd9776c60578b45baf9cf539817ffc65
Unknown <yuwadee.dd58@gmail.com>
2018-01-30 12:27:15 +0700

    เสร็จข้อ 1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,4 +1,17 @@
-int main() {
-        /* code */
-        return 0;
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ofstream myfile ("exmple.txt");
+    if (myfile.is_open())
+    {
+        myfile << "This is a line.\n";
+        myfile << "This is another line.\n";
+        myfile.close();
+    }
+    else cout << "Unable to open file";
+    /* code */
+    return 0;
 }
